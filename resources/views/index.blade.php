bf9524297b93291ffc018cbe6c3359cb19141a66
longo2002 <lombardo.fra2002@gmail.com>
2023-04-19 19:26:19 +0200

    Model & Controller setup

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,24 @@
-<?php
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+
+</head>
 
 @extends('layouts.app')
 
 @section('content')
+
     <div class="container">
       <h1>Elenco dei treni in partenza</h1>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa minus fugit earum molestiae eos repellendus incidunt doloremque! Nisi illo ipsa culpa? Iusto magni corrupti maxime alias quia quidem accusantium fugiat.</p>
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/trains', [TrainController::class, 'index']);
